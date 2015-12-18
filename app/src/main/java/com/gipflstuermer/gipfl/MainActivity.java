d3f446d41ad048baf7321bd02ce0688636cfa88e
Ammon <ammon-mino@web.de>
2015-12-18 09:51:18 +0100

    GPS1

--- app/src/main/java/com/gipflstuermer/gipfl/MainActivity.java
+++ app/src/main/java/com/gipflstuermer/gipfl/MainActivity.java
@@ -1,12 +1,15 @@
 package com.gipflstuermer.gipfl;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
